19c83b1a200c75da175bcf9dff8f81df5194e40b
qiuzhch <apchy!@>
2020-07-08 10:42:23 +0800

    style: 工程侧引入antd 时,图标样式优先级问题

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -29,7 +29,7 @@ const Icon = props => {
     {
       [`${clsPrefix}`]: true,
       [`${clsPrefix}-spin`]: !!spin || type === 'loading',
-      [`${key}`]: Boolean(type)
+      // [`${key}`]: Boolean(type)
     },
     className
   );
