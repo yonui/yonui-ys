a5a20576e9dbc0274f8041c05f9ae8dc8fdf012d
zcyue <973294528@qq.com>
2020-07-24 10:01:16 +0800

    style: remove unuse files.

--- mdf-lang/cli.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-java_path="/Users/jony/workspaces/yonyou/project/yonui-ys/mdf-lang"
-resource_bas="/Users/jony/workspaces/yonyou/project/yonui-ys"
-# resource_path="${resource_bas}/mdf-app/src"
-resource_path="${resource_bas}/src"
-
-
-java -jar /Users/jony/workspaces/yonyou/project/yonui-ys/mdf-lang/ucf-multilang-1.0.6-SNAPSHOT.jar -path ${resource_path} -configpath ${java_path}
