f4f09a7009c5040fe2e32ff49dd695e021016bd0
zcyue <973294528@qq.com>
2020-07-24 12:54:02 +0800

    eslint

--- src/components/TreeSelect/demos/demo.js
+++ src/components/TreeSelect/demos/demo.js
@@ -18,7 +18,7 @@ export default class Demo extends Component {
 
   onChange = (value) => {
     this.setState({ value })
-	}
+  }
 
   render () {
     return (
@@ -31,17 +31,17 @@ export default class Demo extends Component {
         allowClear
         treeDefaultExpandAll
         onChange={this.onChange}
-	    >
+      >
         <TreeNode value='parent 1' title='parent 1' key='0-1'>
           <TreeNode value='parent 1-0' title='parent 1-0' key='0-1-1'>
             <TreeNode value='leaf1' title='my leaf' key='random' />
-            <TreeNode value='leaf2' title='your leaf' key='random1' />  
+            <TreeNode value='leaf2' title='your leaf' key='random1' />
           </TreeNode>
           <TreeNode value='parent 1-1' title='parent 1-1' key='random2'>
             <TreeNode value='sss' title={<b style={{ color: '#08c' }}>sss</b>} key='random3' />
           </TreeNode>
         </TreeNode>
-	    </TreeSelect>
-	  )
-	}
+      </TreeSelect>
+    )
+  }
 }
