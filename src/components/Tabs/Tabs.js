ba11bccb3cfa97aaec1dfe3d88b4590f59954c6d
zcyue <973294528@qq.com>
2020-07-24 17:40:28 +0800

    style: 修改loading 框过大的问题

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-
-import Tabs from 'bee-tabs';
+import React, { Component } from 'react'
+import Tabs from 'bee-tabs'
 
 const tabBarStyleTypeMap = {
   line: 'simple',
   card: 'upborder',
   'editable-card': 'editable-card'
-};
+}
 
 class TabsAdapter extends Component {
     handleChange = (key) => {
       // 疑点：这里的change 事件参数key 有时会是 Proxy 对象，导致页面出错，暂未找到原因，通过下面判断条件临时解决
       if (typeof key !== 'object') {
-        this.props.onChange && this.props.onChange(key);
+        this.props.onChange && this.props.onChange(key)
       }
     }
 
@@ -20,8 +19,9 @@ class TabsAdapter extends Component {
       // Todo：单独把 change 事件抽取出来，而不直接使用 props，因为onchange 会被其他组件（如radio ）触发
       const {
         onChange,
+        className,
         ...others
-      } = this.props;
+      } = this.props
 
       const newProps = {
         ...others,
@@ -32,12 +32,12 @@ class TabsAdapter extends Component {
         },
         tabBarPosition: this.props.tabPosition,
         tabBarStyle: tabBarStyleTypeMap[this.props.type],
-        className:'ys-tabs'
-      };
-      return <Tabs  {...newProps} onChange={this.handleChange}>{this.props.children}</Tabs>
+        className: 'ys-tabs ' + className
+      }
+      return <Tabs {...newProps} onChange={this.handleChange}>{this.props.children}</Tabs>
     }
 }
 
-TabsAdapter.TabPane = Tabs.TabPane;
-TabsAdapter.SearchTabs = Tabs.SearchTabs;
-export default TabsAdapter;
+TabsAdapter.TabPane = Tabs.TabPane
+TabsAdapter.SearchTabs = Tabs.SearchTabs
+export default TabsAdapter
